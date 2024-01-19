6c6aa53c2ac85fe2b9fa6a0b87d3664276abdcb2
jsola84 <141056989+jsola84@users.noreply.github.com>
2024-01-19 21:11:51 +0100

    Update EstructuraBenzineres.java

--- src/main/java/com/mycompany/appeac5p3/EstructuraBenzineres.java
+++ src/main/java/com/mycompany/appeac5p3/EstructuraBenzineres.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.appeac5p3;
+package com.mycompany.appbenzineres;
 
 /**
  *
